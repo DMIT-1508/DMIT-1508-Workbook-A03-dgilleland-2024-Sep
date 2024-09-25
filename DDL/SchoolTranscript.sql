b725da6d27c83c227fd0887ace437f71841f39e0
Dan Gilleland <dgilleland@nait.ca>
2024-09-25 09:41:37 -0600

    Create my first tables

--- DDL/SchoolTranscript.sql
+++ DDL/SchoolTranscript.sql
@@ -18,3 +18,28 @@ USE [SchoolTranscript] -- remaining SQL statements will run against the SchoolTr
 GO
 
 -- Create Tables...
+-- 1) Drop any tables that already exist
+--    Always drop them in the reverse order
+--    that you create them
+DROP TABLE IF EXISTS Courses
+DROP TABLE IF EXISTS Students
+
+-- 2) Setup/Create my tables
+CREATE TABLE Students
+(
+    StudentID       int,
+    GivenName       varchar(50),
+    Surname         varchar(50),
+    DateOfBirth     datetime,
+    Enrolled        bit
+)
+
+CREATE TABLE Courses
+(
+    Number          varchar(10),
+    Name            varchar(50),
+    Credits         decimal(3, 1),
+    Hours           tinyint,
+    Active          bit,
+    Cost            money
+)
